ecc67f62877b59d079480220e0d5b38dc1410cff
DeBiL-png <vechkasov.kirill@mail.ru>
2021-12-02 17:39:10 +0300

    LR_5.2

--- LR5/export.php
+++ LR5/export.php
@@ -7,13 +7,37 @@
 <div class="container mt-5 mb-5 pt-5 pb-5">
     <form method="post" action="export.php">
         <div class="form-group">
-            <label for="path_to_save">Ссылка на файл</label>
-            <br>
-                <a href="<?=$_SERVER['DOCUMENT_ROOT'] . '/LR4/uploads/export.json'?>" download="export.json">
-                    <button class="btn btn-primary">
-                        Скачать экземпляр базы данных
-                    </button>
-                </a>
+
+            <form method="post" action="export.php">
+                <div class="form-group">
+                    <label for="path_to_save">Название вашего JSON-файла</label>
+                    <input type="text" class="form-control" name="path" placeholder="/LR5/upload/export.json" value="<?=isset($_POST['path'])?htmlspecialchars($_POST['path']):"";?>">
+                </div>
+                <button type="submit" class="btn btn-primary mt-1">Скачать</button>
+
+                <?php
+                    if (isset($_POST['path']) && empty($message)) :
+                ?>
+                    <label class="form-control mt-2">Ваш файл доступен по ссылке</label>
+
+                        <a href="<?=$path?>" download="<?=htmlspecialchars($_POST['path'])?>">
+                            <button class="btn btn-primary">
+                                Скачать экземпляр базы данных
+                            </button>
+                        </a>
+                <?php
+                    endif;
+                ?>
+
+                <?php
+                    if (!empty($message)) :
+                ?>
+                    <label class="form-control mt-2"><?=$message?></label>
+                <?php
+                    endif;
+                ?>
+
+            </form>
 
         </div>
     </form>
